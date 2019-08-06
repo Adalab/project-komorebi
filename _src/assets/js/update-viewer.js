916e02e807081b6a619f6cbf5e455b0f40cc4aac
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-08-06 20:57:35 +0200

    add api

--- _src/assets/js/update-viewer.js
+++ _src/assets/js/update-viewer.js
@@ -8,6 +8,7 @@ const tel = document.querySelector(".tel");
 const linkedin = document.querySelector(".linkedin");
 const github = document.querySelector(".Github");
 
+
 //recogemos los campos del visor que queremos actualizar
 const viewerName = document.querySelector(".js-Name");
 const viewerJob = document.querySelector(".js-Job");
@@ -26,43 +27,38 @@ function upgradeName() {
 
 // esta función hace que al borrar el input vuelva a estar relleno con los datos de ejemplo
 function upgradeJob() {
-  //const job = event.currentTarget;
   viewerJob.innerHTML = `${job.value}`;
   if (job.value === "") {
     viewerJob.innerHTML = "Front-end developer";
   }
 }
 
-function upgradeTel(event) {
-  const mail = event.currentTarget;
-  for (let a = 0; a < iconsCheck.length; a++) {}
+function upgradeTel() {
+  for (let a = 0; a < iconsCheck.length; a++) { }
   iconsCheck[0].innerHTML = `<a href="tel:${
     mail.value
-  }" target="_blank"><i class="fas fa-mobile-alt"></i></a>`;
+    }" target="_blank"><i class="fas fa-mobile-alt"></i></a>`;
 }
 
-function upgradeMail(event) {
-  const mail = event.currentTarget;
-  for (let a = 0; a < iconsCheck.length; a++) {}
+function upgradeMail() {
+  for (let a = 0; a < iconsCheck.length; a++) { }
   iconsCheck[1].innerHTML = `<a href="mailto:${
     mail.value
-  }" target="_blank"><i class="far fa-envelope"></i></a>`;
+    }" target="_blank"><i class="far fa-envelope"></i></a>`;
 }
 
-function upgradeLinkedin(event) {
-  const mail = event.currentTarget;
-  for (let a = 0; a < iconsCheck.length; a++) {}
+function upgradeLinkedin() {
+  for (let a = 0; a < iconsCheck.length; a++) { }
   iconsCheck[2].innerHTML = `<a href="https://www.${
     mail.value
-  }" target="_blank"><i class="fab fa-linkedin-in"></i></a>`;
+    }" target="_blank"><i class="fab fa-linkedin-in"></i></a>`;
 }
 
-function upgradeGithub(event) {
-  const mail = event.currentTarget;
-  for (let a = 0; a < iconsCheck.length; a++) {}
+function upgradeGithub() {
+  for (let a = 0; a < iconsCheck.length; a++) { }
   iconsCheck[3].innerHTML = `<a href="https://github.com/${
     mail.value
-  }" target="_blank"><i class="fab fa-github-alt"></i></a>`;
+    }" target="_blank"><i class="fab fa-github-alt"></i></a>`;
 }
 
 //Creamos una función para almacenar los datos introducidos en el localStorage.
@@ -99,7 +95,7 @@ const upgradeDataViewer = () => {
   upgradeLinkedin();
   upgradeGithub();
 };
-//para cada input, cuando occurra su evento, se ejecuta la función que modificará su contenido en el visor
+//para cada input, cuando occurra su o, se ejecuta la función que modificará su contenido en el visor
 fullName.addEventListener("keyup", upgradeDataViewer);
 job.addEventListener("keyup", upgradeDataViewer);
 mail.addEventListener("keyup", upgradeDataViewer);
@@ -108,3 +104,34 @@ linkedin.addEventListener("keyup", upgradeDataViewer);
 github.addEventListener("keyup", upgradeDataViewer);
 formData.addEventListener("keyup", handleFormData);
 getLocalStorage();
+
+
+//API
+
+const shareButton = document.querySelector('.js-shareButton');
+
+
+
+const data = {
+  "palette": 1,
+  "name": fullName.value,
+  "job": job.value,
+  "phone": tel.value,
+  "email": mail.value,
+  "linkedin": linkedin.value,
+  "github": github.value,
+  "photo": "data:image/png;base64,2342ba..."
+}
+
+function conectApi() {
+  console.log(data)
+  fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/', {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+    .then()
+}
+
+
+
+shareButton.addEventListener('click', conectApi);
